65d7c79685b43374f963777db06df145b7b4a5df
ilyayn11 <ilyayn11@gmail.com>
2026-04-15 21:13:36 +0000

    add seo and metrika

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,28 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://arhangelpushkino.ru',
+
   // Разрешаем Astro работать на нашем домене
   server: {
     allowedHosts: true
   },
+
   build: {
     // Эта команда — наше крайнее средство. Она заставляет Astro ВСЕГДА встраивать CSS внутрь HTML.
     inlineStylesheets: 'always', 
   },
+
   vite: {
     plugins: [tailwindcss()],
     build: {
       cssCodeSplit: false, // Соберем все стили в один кусок для надежности
       assetsInlineLimit: 10240, // Вшиваем мелкие картинки и шрифты прямо в код
     }
-  }
+  },
+
+  integrations: [sitemap()]
 });
